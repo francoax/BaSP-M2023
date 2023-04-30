0cd05542cf7811cfa6fb34a17787f379f02b34dd
francoax <francoa.duarte2001@gmail.com>
2023-04-30 20:09:51 -0300

    BaSP-W07-2023: signup localStorage and modal done.

--- Week-07/js/sign-up.js
+++ Week-07/js/sign-up.js
@@ -33,7 +33,7 @@ var validateForm = function(form) {
           return false;
         }
         if(input.value && !hasOnlyLetters(input.value)) {
-          this.errorMessage = 'Name can contain only letters';
+          this.errorMessage = 'Name must be only letters';
           return false;
         }
         return true;
@@ -109,11 +109,11 @@ var validateForm = function(form) {
           return false;
         }
         if(input.value && input.value.length !== 10) {
-          this.errorMessage = 'Telephone not valid';
+          this.errorMessage = 'Telephone must have 10 numbers';
           return false;
         }
         if(input.value && !hasOnlyNumbers(input.value)) {
-          this.errorMessage = 'Telephone can only have numbers';
+          this.errorMessage = 'Telephone must be only numbers';
           return false;
         }
         return true;
@@ -240,32 +240,32 @@ var validateForm = function(form) {
             this.errorMessage = 'Password must not have empty spaces';
             return false;
           }
-          if(input.value.length < 8) {
-            this.errorMessage = 'Password must have 8 or more caracters';
+          if(input.value.length < 7) {
+            this.errorMessage = 'Password must have 7 or more caracters';
             return false
           }
-          var hasMayus = false;
-          for (var i = 0; i < input.value.length; i++) {
-            var char = input.value.charCodeAt(i);
-            if((char >= 65 && char <= 90)) {
-              hasMayus = true;
-            }
-          }
-          if(!hasMayus) {
-            this.errorMessage = 'Password must contain one uppercase letter';
-            return false;
-          }
-          var numbers = 0;
-          for (var i = 0; i < input.value.length; i++) {
-            var char = input.value.charCodeAt(i);
-            if((char >= 48 && char <= 57)) {
-              numbers++
-            }
-          }
-          if(numbers < 2) {
-            this.errorMessage = 'Password must contain two or more numbers';
-            return false;
-          }
+          // var hasMayus = false;
+          // for (var i = 0; i < input.value.length; i++) {
+          //   var char = input.value.charCodeAt(i);
+          //   if((char >= 65 && char <= 90)) {
+          //     hasMayus = true;
+          //   }
+          // }
+          // if(!hasMayus) {
+          //   this.errorMessage = 'Password must contain one uppercase letter';
+          //   return false;
+          // }
+          // var numbers = 0;
+          // for (var i = 0; i < input.value.length; i++) {
+          //   var char = input.value.charCodeAt(i);
+          //   if((char >= 48 && char <= 57)) {
+          //     numbers++
+          //   }
+          // }
+          // if(numbers < 2) {
+          //   this.errorMessage = 'Password must contain two or more numbers';
+          //   return false;
+          // }
         }
         return true;
       }
@@ -353,9 +353,98 @@ var validateForm = function(form) {
           message += label.trim() + ': ' + date + '\n';
         }
       }
+
+      var name = formElement.querySelector('#name').value;
+      var lastname = formElement.querySelector('#lastname').value;
+      var dni = formElement.querySelector('#dni').value;
+      var dob = formElement.querySelector('#date-born').value;
+      dobSplitted = dob.split('-');
+      var tel = formElement.querySelector('#tel').value;
+      var direction = formElement.querySelector('#direction').value;
+      var city = formElement.querySelector('#city').value;
+      var zip = formElement.querySelector('#postal-code').value;
+      var email = formElement.querySelector('#email').value;
+      var password = formElement.querySelector('#password').value;
+
+      fetch('https://api-rest-server.vercel.app/signup?'
+      + 'name=' + name
+      + '&lastName=' + lastname
+      + '&dni=' + dni
+      + '&dob=' + dobSplitted[1] + '/' + dobSplitted[2] + '/' + dobSplitted[0]
+      + '&phone=' + tel
+      + '&address=' + direction
+      + '&city=' + city
+      + '&zip=' + zip
+      + '&email=' + email
+      + '&password=' + password
+      )
+      .then(function(response) {
+        return response.json()
+      })
+      .then(function(data) {
+        if(data.success) {
+          showModalMsg('Successful request!', data.msg);
+          localStorage.setItem('name', name);
+          localStorage.setItem('lastname', lastname);
+          localStorage.setItem('dni', dni);
+          localStorage.setItem('dob', dob);
+          localStorage.setItem('phone', tel);
+          localStorage.setItem('address', direction);
+          localStorage.setItem('city', city);
+          localStorage.setItem('zip', zip);
+          localStorage.setItem('email', email);
+        } else {
+          let msg = '';
+          data.errors.forEach(function(error) {
+            msg += error.msg + '\n';
+          })
+          throw new Error(msg);
+        }
+      })
+      .catch(function(error) {
+        showModalMsg('Something wents wrong', error.message);
+      })
       alert('Nice, you got it: \n' + message);
     }
   });
 }
 
 validateForm('#sign-up-form');
+
+window.onload = () => {
+  document.querySelector('#name').value = localStorage.getItem('name');
+  document.querySelector('#lastname').value = localStorage.getItem('lastname');
+  document.querySelector('#dni').value = localStorage.getItem('dni');
+  document.querySelector('#date-born').value = localStorage.getItem('dob');
+  document.querySelector('#tel').value = localStorage.getItem('phone');
+  document.querySelector('#direction').value = localStorage.getItem('address');
+  document.querySelector('#city').value = localStorage.getItem('city');
+  document.querySelector('#postal-code').value = localStorage.getItem('zip');
+  document.querySelector('#email').value = localStorage.getItem('email');
+}
+
+var crossModalClose = document.querySelector('.modal-close');
+var modal = document.querySelector('#modal-msg');
+var btnModalClose = modal.querySelector('#btn-ok');
+
+crossModalClose.addEventListener('click', function(event) {
+  modal.style.display = 'none';
+})
+
+btnModalClose.addEventListener('click', function(event) {
+  modal.style.display = 'none';
+})
+
+window.onclick = function(event) {
+  if(event.target === modal) {
+    modal.style.display = 'none';
+  }
+}
+
+function showModalMsg(title, msg) {
+  var modalTitle = modal.querySelector('.modal-title > p');
+  var content = modal.querySelector('#modal-details');
+  modal.style.display = 'block';
+  modalTitle.innerText = title;
+  content.innerText = msg;
+}
